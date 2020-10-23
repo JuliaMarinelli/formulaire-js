c644fb2bacf5edd9f7ad427681a0f4cfe9536325
Julia <julia_marinelli@hotmail.fr>
2020-10-23 09:33:19 +0200

    add main.js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const submit = document.getElementById("submitButton")
+const password = document.getElementById("password")
+const name = document.getElementById("name")
+const email = document.getElementById("email")
+
+let passwordIsFill;
+let nameIsFill;
+let emailIsFill;
+
+window.onload = function(){
+    passwordIsFill = password.value.toString().length !== 0
+    nameIsFill = name.value.toString().length !== 0
+    emailIsFill = email.value.toString().length !== 0
+    submit.disabled = !passwordIsFill || !nameIsFill || !emailIsFill
+}
+
+password.addEventListener('keyup', (e)=>{
+    passwordIsFill = e.target.value.toString().length !== 0
+    submitUpdate();
+})
+name.addEventListener('keyup', (e)=>{
+    nameIsFill = e.target.value.toString().length !== 0
+    submitUpdate();
+})
+email.addEventListener('keyup', (e)=>{
+    emailIsFill = e.target.value.toString().length !== 0
+    submitUpdate();
+})
+
+submitUpdate = function() {
+    submit.disabled = !passwordIsFill || !nameIsFill || !emailIsFill
+}
