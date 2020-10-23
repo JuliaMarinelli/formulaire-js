f27b2414d13bd960526c6b1c9cd47b8279d42dfc
Julia <julia_marinelli@hotmail.fr>
2020-10-23 11:51:53 +0200

    add error messages

--- js/main.js
+++ js/main.js
@@ -4,37 +4,92 @@ const name = document.getElementById("name")
 const email = document.getElementById("email")
 const form = document.getElementById('formId')
 
+const passwordError = document.getElementById("passwordError")
+const nameError = document.getElementById("nameError")
+const emailError = document.getElementById("emailError")
+
 let passwordIsFill;
 let nameIsFill;
 let emailIsFill;
 
 window.onload = function(){
-    passwordIsFill = password.value.toString().length !== 0
-    nameIsFill = name.value.toString().length !== 0
-    emailIsFill = email.value.toString().length !== 0
+    passwordUpdate();
+    nameUpdate();
+    emailUpdate();
     submitUpdate();
+    removeErrors();
 }
 
 password.addEventListener('keyup', (e)=>{
-    passwordIsFill = e.target.value.toString().length !== 0
+    passwordUpdate();
     submitUpdate();
 })
 name.addEventListener('keyup', (e)=>{
-    nameIsFill = e.target.value.toString().length !== 0
+    nameUpdate();
     submitUpdate();
 })
 email.addEventListener('keyup', (e)=>{
-    emailIsFill = e.target.value.toString().length !== 0
+    emailUpdate();
     submitUpdate();
 })
 
+passwordUpdate = function(){
+    passwordIsFill = password.value.toString().length !== 0
+    
+    if(passwordIsFill) {
+        password.classList.add('valid')
+        password.classList.remove('invalid')
+    }
+    else { 
+        password.classList.add('invalid')
+        password.classList.remove('valid')
+    }
+}
+
+nameUpdate = function(){
+    nameIsFill = name.value.toString().length !== 0
+    if(nameIsFill) {
+        name.classList.add('valid')
+        name.classList.remove('invalid')
+    }
+    else { 
+        name.classList.add('invalid')
+        name.classList.remove('valid')
+    }
+}
+
+emailUpdate = function(){
+    emailIsFill = email.value.toString().length !== 0
+    
+    if(emailIsFill) {
+        email.classList.add('valid')
+        email.classList.remove('invalid')
+    }
+    else { 
+        email.classList.add('invalid')
+        email.classList.remove('valid')
+    }
+}
+
 submitUpdate = function() {
-    submit.disabled = !passwordIsFill || !nameIsFill || !emailIsFill
+    submit.disabled = !passwordIsFill || !nameIsFill || !emailIsFill;
+}
+
+removeErrors = function() {
+    passwordError.classList.add('hide');
+    emailError.classList.add('hide');
+    nameError.classList.add('hide');
 }
 
 form.addEventListener("submit", function (e) {
     if(!passwordIsFill || !nameIsFill || !emailIsFill) {
         alert("NOOOOON !")
         e.preventDefault()
+        if(!passwordIsFill)
+            passwordError.classList.remove('hide')
+        if(!nameIsFill)
+            nameError.classList.remove('hide')
+        if(!emailIsFill)
+            emailError.classList.remove('hide')
     }
 })
